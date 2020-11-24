5a8604d6ea790c6f98864e1972c3c4705cf91dbe
BXSS101 <akarawinb@hotmail.com>
2020-11-24 16:49:09 +0700

    Update PC1

---
+++ pc1/FM_Rx.cpp
@@ -0,0 +1,72 @@
+#include "FM_Rx.h"
+
+FM_rx::FM_rx(float freq)
+{
+    sbi(ADCSRA, ADPS2); // this for increase analogRead speed
+    cbi(ADCSRA, ADPS1);
+    cbi(ADCSRA, ADPS0);
+
+    Wire.begin();
+    radio.setFrequency(freq);
+}
+
+uint8_t FM_rx::receiveFrame(int timeout)
+{
+    unsigned long start = millis();
+    uint8_t aData[40];
+    int cIdx = 0;
+
+    while (millis() - start < timeout)
+    {
+        uint16_t tmp = analogRead(A2);
+        if (tmp - r_slope and prev < r_slope and !check_amp)
+        {
+            check_amp = true;
+            if (!check_baud)
+            {
+                baud_begin = micros();
+                bit_check++;
+            }
+        }
+
+        if (tmp < r_slope and check_baud)
+        {
+            if (micros() - baud_begin > baudTime)
+            {
+                uint16_t last = (int(ceil(float(count - 1) / 5)) & 3) << (bit_check * 2);
+                ;
+                data |= last;
+                baud_check++;
+                if (baud_check == 4)
+                {
+                    //return char(data);
+                    aData[cIdx] = data;
+                    cIdx++;
+                    data = 0;
+                    baud_check = 0;
+                    bit_check = -1;
+                }
+                check_baud = false;
+                count = 0;
+            }
+        }
+        if (micros() - baud_begin > 40000)
+        {
+            data = 0;
+            baud_check = 0;
+            bit_check = -1;
+            check_amp = false;
+            check_baud = false;
+            count = 0;
+        }
+
+        if (tmp > r_slope and check_amp)
+        {
+            count++;
+            check_baud = true;
+            check_amp = false;
+        }
+        prev = tmp;
+    }
+    return aData;
+}
---
+++ pc1/FM_Rx.h
@@ -0,0 +1,36 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <TEA5767Radio.h>
+
+#ifndef cbi
+#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
+#endif
+#ifndef sbi
+#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+#endif
+
+//edit this number
+#define r_slope 300
+
+class FM_rx
+{
+public:
+    int receiveFrame(int timeout);
+    FM_rx(float freq);
+
+private:
+    int prev = 0;
+    int count = 0;
+
+    uint16_t baud_check = 0;
+    uint16_t data = 0;
+    uint16_t bit_check = -1;
+
+    bool check_amp = false;
+    bool check_baud = false;
+
+    uint32_t baud_begin = 0;
+
+    int baudTime = 10000;
+    TEA5767Radio radio = TEA5767Radio();
+};
