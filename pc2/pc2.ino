561d8a0d8f7a93246a21a69aef4841c4056d7cf2
j4m3ee <j4m3ee01@gmail.com>
2020-11-25 15:35:02 +0700

    backup

--- pc2/pc2.ino
+++ pc2/pc2.ino
@@ -14,6 +14,7 @@ String data[3][21];
 String tmpData;
 uint8_t data_index = 0;
 uint8_t idx = 0;
+char tmpOpr;
 
 char state[3][10] = {"INIT", "WAIT", "SENDING"};
 String nowState = "INIT";
@@ -56,15 +57,21 @@ void loop()
     connection();
     delay(500);
     Serial.write('S');
+    receiver -> Clear();
 
     //    writeString();
     nowState = "WAIT";
   }
   if (nowState == "WAIT") {
-    char tmpOpr = receiver -> Receive()[0];
+    tmpOpr = receiver -> Receive()[0];
+        Serial.write(tmpOpr);
+
     if (tmpOpr == 'L') {
+      receiver -> Clear();
+      //transmitter -> fskTransmit("A");
       bin[0] = director -> Capture(angle[idx]);
       idx = 0;
+      data[idx][data_index] = "";
       for (uint8_t j = 0; j < bin[idx].length(); j++) {
         if (bin[idx][j] == ',') {
           data_index++;
@@ -77,7 +84,11 @@ void loop()
       tmpData = bin[idx];
       nowState = "SENDING";
     } else if (tmpOpr == 'C') {
+      receiver -> Clear();
+      
+      //transmitter -> fskTransmit("A");
       idx = 1;
+      data[idx][data_index] = "";
       bin[idx] = director -> Capture(angle[idx]);
       for (uint8_t j = 0; j < bin[idx].length(); j++) {
         if (bin[idx][j] == ',') {
@@ -91,10 +102,13 @@ void loop()
       tmpData = bin[idx];
       nowState = "SENDING";
     } else if (tmpOpr == 'R') {
+      receiver -> Clear();
+      //transmitter -> fskTransmit("A");
       idx = 2;
       bin[idx] = director -> Capture(angle[idx]);
-
+      data[idx][data_index] = "";
       for (uint8_t j = 0; j < bin[idx].length(); j++) {
+        
         if (bin[idx][j] == ',') {
           data_index++;
         } else {
@@ -107,44 +121,24 @@ void loop()
       nowState = "SENDING";
     }
     else if (tmpOpr == 'T') {
-      flushData();
-      for (uint8_t i = 0; i < 3; i++) {
-        bin[i] = director -> Capture(angle[i]);
-      }
-      for (uint8_t i = 0; i < 3; i++) {
-        for (uint8_t j = 0; j < bin[i].length(); j++) {
-          if (bin[i][j] == ',') {
-            data_index++;
-          } else {
-            data[i][data_index] += bin[i][j];
-          }
-        }
-        data_index = 0;
-      }
-      connection();
       delay(500);
-      Serial.write('S');
+      transmitter -> fskTransmit("A");
+      flushData();
+      nowState = "INIT";
     }
+    receiver -> Clear();
     //receiver -> receiveFrame(100);
   }
 
   if (nowState == "SENDING") {
-
-    String sentTmp = "";
-    for (int i = 0; i < 3; i++) {
-      sentTmp += String(i);
-      sentTmp += "|";
-      for (int j = 0; j < 7; j++) {
-        sentTmp += data[idx][(7 * i) + j];
-      }
-      sentPackData(sentTmp);
-      Serial.write('z');
-    }
-
+    sentPackData(data[idx][0]);
+    delay(500);
+    Serial.write('z');
     delay(500);
     tmpData = "";
     Serial.write('S');
     nowState = "WAIT";
+
   }
 }
 
@@ -163,7 +157,7 @@ void connection() {
       Serial.write('l');
       nowState = "WAIT";
       delay(500);
-      break;
+      return;      
     }
   }
   delay(500);
@@ -175,14 +169,15 @@ void sentPackData(String data) {
   writeString(data);
   transmitter -> fskTransmit(data);
   Serial.write('w');
-  
+
   long t = millis();
   while (waitAck()) {
     if (millis() - t > 5000) {
       Serial.write('l');
+      tmpOpr = '-';
       nowState = "WAIT";
       delay(500);
-      break;
+      return;   
     }
   }
   delay(500);
@@ -204,10 +199,41 @@ void writeString(String str) {
   }
 }
 
-void flushData(){
-  for(int i=0;i<3;i++){
-    for(int j=0;j<21;j++){
+void flushData() {
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 21; j++) {
       data[i][j] = "";
     }
   }
+}
+
+String addCheckSum(String in) {
+  String to;
+  int s = in.length();
+  char tem[s];
+  in.toCharArray(tem,s);
+  int i;
+  int sum = 0;
+  to = 2 + in;
+  for (i = 0; i < s; i ++) {
+    sum += tem[i];
+    if (sum >= 256) {
+      sum = sum - 255;
+    }
+  }
+  char comply = 16 * 16 - sum - 1;
+  to += comply;
+  //Serial.println(to);
+  return to;
+}
+
+String dataDepack(String in){
+  String out;
+  uint8_t frameSize = in.length();
+  char tem[frameSize];
+  in.toCharArray(tem,in.length());
+  for(int i=1;i<frameSize-1;i++){
+    out += tem[i];
+  }
+  return out;
 }
